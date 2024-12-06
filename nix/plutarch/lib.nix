88601d3f90ea0a5795a3017182cd17a8f91ab52d
t4ccer <t4ccer@gmail.com>
2024-12-06 13:30:55 -0700

    Bump plutarch

--- nix/plutarch/lib.nix
+++ nix/plutarch/lib.nix
@@ -1,26 +1,13 @@
 { fetchFromGitHub
 , mkHaskellPackage
-, applyPatches
-, fetchpatch
 }:
 
 let
-  plutarchPackage = applyPatches {
-    name = "plutarch-patched";
-    src = fetchFromGitHub {
-      owner = "Plutonomicon";
-      repo = "plutarch-plutus";
-      rev = "288d9140468ae98abe1c9a4c0bb1c19a82eb7cd6"; # branch: master
-      hash = "sha256-aeaZMW5Y3r5GdSyrfrrKOuGahcL5MVkDUNggunbmtv0=";
-    };
-
-    patches = [
-      # https://github.com/Plutonomicon/plutarch-plutus/pull/650
-      (fetchpatch {
-        url = "https://github.com/Plutonomicon/plutarch-plutus/commit/7256acb8db3230d2453460f0358582283c69da5f.patch";
-        hash = "sha256-y/F1ZwLDC5E4vh8F+JTQStHJsQ1ZEe9LIZcwSGMSUek=";
-      })
-    ];
+  plutarchPackage = fetchFromGitHub {
+    owner = "Plutonomicon";
+    repo = "plutarch-plutus";
+    rev = "96de13396a469f652b48735c80b8f55b37b8b396"; # branch: staging
+    hash = "sha256-NFYBw/QGvsE58fJqlTP2+nFPsWnsI9rJbeHSOEeOhUw=";
   };
 
   cardanoPackages = fetchFromGitHub {
