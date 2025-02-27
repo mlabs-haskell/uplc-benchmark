6578f4808249ac1300beff0c7e3e1ddfe2ef4c91
t4ccer <t4ccer@gmail.com>
2025-02-27 09:08:13 -0700

    Remove unused nix follows

--- flake.nix
+++ flake.nix
@@ -9,7 +9,6 @@
     pre-commit-hooks-nix = {
       url = "github:cachix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.nixpkgs-stable.follows = "nixpkgs";
     };
     hci-effects = {
       url = "github:hercules-ci/hercules-ci-effects";
