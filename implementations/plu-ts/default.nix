e75e30ae3ffdcfb520e4a0a1dd8affb28b722ecc
t4ccer <t4ccer@gmail.com>
2025-02-28 06:15:32 -0700

    Nixify plu-ts build

--- implementations/plu-ts/default.nix
+++ implementations/plu-ts/default.nix
@@ -1,10 +1,29 @@
 {
-  perSystem = { pkgs, config, ... }: {
+  perSystem = { pkgs, lib, config, ... }: {
     devShells.plu-ts-implementation = pkgs.mkShell {
       shellHook = config.pre-commit.installationScript;
       nativeBuildInputs = [
         pkgs.nodejs
       ];
+    };
+
+    packages.plu-ts-implementation-compiled = pkgs.buildNpmPackage {
+      name = "plu-ts-compiled";
+      src = builtins.filterSource (path: _: !(lib.strings.hasSuffix ".nix" path)) ./.;
+
+      npmDepsHash = "sha256-/v/zP6jhKmBjpXIJ9yW1gFFYik89zONAs6CDAHg0c68=";
+
+      npmBuildScript = "start";
+
+      installPhase = ''
+        runHook preInstall
+
+        cp -r out $out
+
+        runHook postInstall
+      '';
+
+      passthru.versions.plu-ts = (builtins.fromJSON (builtins.readFile ./package-lock.json)).packages."node_modules/@harmoniclabs/plu-ts".version;
     };
   };
 }
