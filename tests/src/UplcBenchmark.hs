9eae7a7ab93712927e88e20e0a785d8a7b797caa
t4ccer <t4ccer@gmail.com>
2025-03-20 12:23:41 -0600

    Disable opshin and plutusTx

--- tests/src/UplcBenchmark.hs
+++ tests/src/UplcBenchmark.hs
@@ -80,6 +80,6 @@ implementations :: [Implementation]
 implementations =
   [ Implementation "Plutarch" "UPLC_BENCHMARK_BIN_PLUTARCH"
   , Implementation "Aiken" "UPLC_BENCHMARK_BIN_AIKEN"
-  , Implementation "PlutusTx" "UPLC_BENCHMARK_BIN_PLUTUS_TX"
-  , Implementation "Opshin" "UPLC_BENCHMARK_BIN_OPSHIN"
+  -- , Implementation "PlutusTx" "UPLC_BENCHMARK_BIN_PLUTUS_TX"
+  -- , Implementation "Opshin" "UPLC_BENCHMARK_BIN_OPSHIN"
   ]
