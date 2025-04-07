94b7c54445d3f3100ab0121e80fa0eab8d8be049
t4ccer <t4ccer@gmail.com>
2025-04-06 22:35:41 -0600

    Update fourmolu

--- nix/fourmolu/default.nix
+++ nix/fourmolu/default.nix
@@ -7,7 +7,7 @@
   src = fetchFromGitHub {
     owner = "fourmolu";
     repo = "fourmolu";
-    rev = "v0.13.1.0";
-    hash = "sha256-abUK9KdvVI7di84X/L3vHZM97pOsciyx503aDjUnoc4=";
+    rev = "v0.18.0.0";
+    hash = "sha256-VygaYu/sK61TFaKXnsfC+GaXqRccb1Ue/4Ut5vbdpvA=";
   };
 }).packages."fourmolu:exe:fourmolu"
