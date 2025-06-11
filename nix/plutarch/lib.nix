cdbc34f5573cb6ac15b960e10f3bb408ff6cf4e2
t4ccer <t4ccer@gmail.com>
2025-06-11 14:03:42 +0200

    Move cardano deps to flake inputs

--- nix/plutarch/lib.nix
+++ nix/plutarch/lib.nix
@@ -1,22 +1,7 @@
-{ fetchFromGitHub
-, mkHaskellPackage
+{ mkHaskellPackage
+, plutarch
+, cardanoPackages
 }:
-
-let
-  plutarchPackage = fetchFromGitHub {
-    owner = "Plutonomicon";
-    repo = "plutarch-plutus";
-    rev = "7b346d00596531d3682204e226f7457d51849a21";
-    hash = "sha256-k6Sm8DqSvaRDbNsIivzbw/1JrWuiDT7xoVll9ZqGgXM=";
-  };
-
-  cardanoPackages = fetchFromGitHub {
-    owner = "IntersectMBO";
-    repo = "cardano-haskell-packages";
-    rev = "5f473b88ec38382dd9d05bdf50ecc03a26461fa8"; # branch: repo
-    hash = "sha256-Ts/+6Xlh++ePF4VjWIVdvkmnBJR5Vde1c/0KqraUv/8=";
-  };
-in
 
 args:
 mkHaskellPackage (args // {
@@ -25,7 +10,7 @@ mkHaskellPackage (args // {
   } // (args.externalRepositories or { });
 
   externalDependencies = [
-    "${plutarchPackage}"
-    "${plutarchPackage}/plutarch-ledger-api"
+    "${plutarch}"
+    "${plutarch}/plutarch-ledger-api"
   ] ++ (args.externalDependencies or [ ]);
 })
